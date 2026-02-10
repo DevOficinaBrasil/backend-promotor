cc1fff4f0ff21e823a61209d669622b88ee95861
Kaaaioh <85952871+Kaaaioh@users.noreply.github.com>
2026-02-10 14:20:52 -0300

    hotifx

--- api.ts
+++ api.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import modeloRoutes from "./routes/ModeloRoute";
 import campanhaRoutes from "./routes/CampanhaRoute";
 import promotorRoutes from "./routes/PromotorRoute";
 
 const routes = (app: express.Application) => {
-  app.use("/modelo", modeloRoutes);
   app.use("/campanha", campanhaRoutes);
   app.use("/promotor", promotorRoutes);
 };
